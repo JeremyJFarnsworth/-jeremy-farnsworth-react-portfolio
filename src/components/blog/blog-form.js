d1ef2da9a66a2e8cb5911ed150eb57621a97fb01
JeremyJFarnsworth <jdog1123@gmail.com>
2022-09-12 16:10:27 -0600

    9/12 4 update

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import RichTextEditor from '../forms/rich-text-editor';
+
 export default class BlogForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             title: "",
-            blog_status: ""
-        }
+            blog_status: "",
+            content: ""
+        };
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this)
+    }
+
+    handleRichTextEditorChange(content) {
+        this.setState({
+            content
+        });
     }
 
     buildForm() {
@@ -19,6 +29,7 @@ export default class BlogForm extends Component {
 
         formData.append("portfolio_blog[title]", this.state.title);
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+        formData.append("portfolio_blog[content]", this.state.content);
 
         return formData;
     }
@@ -29,12 +40,16 @@ export default class BlogForm extends Component {
             this.buildForm(),
             { withCredentials: true }
         ).then(response => {
-            this.props.handleSuccessfulBlogFormSubmission(response.data.portfolio_blog);
-
             this.setState({
                 title: "",
-                blog_status: ""
+                blog_status: "",
+                content: ""
             })
+
+            this.props.handleSuccessfulBlogFormSubmission(
+                response.data.portfolio_blog
+            );
+
         }).catch(error => {
             console.log("handleSubmit for blog error", error);
         });
@@ -69,6 +84,13 @@ export default class BlogForm extends Component {
                         value={this.state.blog_status}
                     />
                 </div>
+
+                <div className='one-column'>
+                    <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange} />
+                </div>
+
+
+
                 <button className='btn'>Save</button>
 
             </form>
